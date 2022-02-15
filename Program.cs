d6d35bbddb0b10c09592f1807ad3d2007b5b3172
Valdir Lima <valdirlima508@gmail.com>
2022-02-15 13:16:24 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            /*int answer = 4;
+            string response;
+
+            if (answer > 9)
+            {
+                response = answer + "is less than 9";
+            }
+            else
+            {
+                response = answer + "greater than or equal to nine";
+            }*/
+
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);    
+        }
+    }
+}
